1458b5649bae1b67f297010df69214ca104703ac
SunnyF419 <ftysunny@outlook.com>
2026-05-10 17:42:27 +0800

    Update music-manifest.js

--- music-manifest.js
+++ music-manifest.js
@@ -3,8 +3,8 @@ window.MUSIC_TRACKS = [
     id: "left-tail-side-answers",
     title: "Left-Tail Side Answers",
     artist: "AI Music",
-    src: "./left-lail-side-answers.mp3",
-    captions: "./left-lail-side-answers.lrc"
+    src: "./left-tail-side-answers.mp3",
+    captions: "./left-tail-side-answers.lrc"
   },
   {
     id: "zuo-wei-dui-bai",
